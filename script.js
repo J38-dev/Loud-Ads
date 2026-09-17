c587d3ee4f13809868d3e0b7500a74db1e5e730f
J38-dev <jayxdenjohnson781@gmail.com>
2026-09-17 06:53:34 +0200

    Refactor magnetic button and interaction logic

--- script.js
+++ script.js
@@ -2,39 +2,6 @@
    MAGNETIC BUTTON
 ========================= */
 
-const btn = document.querySelector(".magnetic");
-
-btn.addEventListener("mousemove",(e)=>{
-const rect = btn.getBoundingClientRect();
-const x = e.clientX - rect.left - rect.width/2;
-const y = e.clientY - rect.top - rect.height/2;
-
-btn.style.transform = `translate(${x*0.2}px,${y*0.2}px)`;
-});
-
-btn.addEventListener("mouseleave",()=>{
-btn.style.transform = "translate(0,0)";
-});
-
-const images = document.querySelectorAll(".masonry img");
-const images = document.querySelectorAll(".masonry img");
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
-
-if (lightbox && lightboxImg) {
-
-  images.forEach(img=>{
-    img.addEventListener("click",()=>{
-      lightbox.style.display="flex";
-      lightboxImg.src=img.src;
-    });
-  });
-
-  lightbox.addEventListener("click",()=>{
-    lightbox.style.display="none";
-  });
-
-   }
 
 window.addEventListener("scroll",()=>{
 let scroll =
